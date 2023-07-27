af36fadb2249d4e2aef4ee7c4eeb47fa58493461
bbird5063 <bbird5063@gmail.com>
2023-07-27 16:39:06 +0300

    8:08 error

---
+++ index.ts
@@ -0,0 +1,34 @@
+class Rectangle {
+    private _width;
+    private _height;
+
+    constructor(w, h) {
+        this._width = w;
+        this._height = h;
+    }
+
+    calcArea() {
+        return this._width * this._height;
+    }
+
+    get width() {
+        return this._width;
+    }
+
+    set width(value) {
+        if(value <+ 0) {
+            this._width = 1;    
+        }
+        else {
+            this._width = value;
+        }
+    }
+}
+
+const rect = new Rectangle(5, 10);
+/*const rect2 = new Rectangle(w:52, h:102);
+const rect3 = new Rectangle(w:5, h:102);*/
+
+rect._width = -2;
+
+console.log('ПРОВЕРКА!!!');
